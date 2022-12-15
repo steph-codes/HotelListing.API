29e01f1a4a8f705d05741c5befa4893ff1dcc9a8
steph-codes <ogundele370@gmail.com>
2022-12-15 22:13:04 +0100

    Seeding Models from DbContext Files on Model Creating part1

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id =1,
+                    Name = "Nigeria", 
+                    ShortName ="NG"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Morocco",
+                    ShortName = "MR"
+                }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Lagos",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 2,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "CR7 Pestana",
+                    Address = "Marrakech Morroco",
+                    CountryId = 3,
+                    Rating = 4
+                }
+            );
+        }
+    }
+}
